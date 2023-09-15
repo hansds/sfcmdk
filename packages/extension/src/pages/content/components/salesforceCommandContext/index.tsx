4f4c06fc68eda781490bfa6bb2e1862db53cd070
Hans De Smet <hello@hansdesmet.be>
2023-09-15 02:46:58 +0200

    chore: clean up unused references

--- packages/extension/src/pages/content/components/salesforceCommandContext/index.tsx
+++ packages/extension/src/pages/content/components/salesforceCommandContext/index.tsx
@@ -1,32 +1,15 @@
 import React, { useState } from "react";
-// import { useTheme } from "next-themes";
-import {
-  getOrgIdFromDocument,
-  isProbablySalesforceId,
-} from "../../../../shared/content/utils";
+import { getOrgIdFromDocument } from "../../../../shared/content/utils";
 import { MessageType } from "../../../../shared/messaging";
 import { sendTypedMessage } from "../../../../shared/messaging/content";
 import { JSONArray } from "../../../../shared/messaging/types";
 import SalesforceCommand from "../salesforceCommand";
 
 export default function SalesforceCommandContext() {
-  // const { resolvedTheme: theme } = useTheme();
   const orgId = getOrgIdFromDocument(document);
-
-  const [loading, setLoading] = React.useState(false);
-  const [value, setValue] = React.useState("");
-  const [recordId, setRecordId] = React.useState("");
-  const [search, setSearch] = React.useState("");
-  const [isMetaKeyActive, setIsMetaKeyActive] = React.useState(false);
-  const [notification, setNotification] = React.useState("");
 
   const [users, setUsers] = useState<JSONArray>([]);
   const [customObjects, setCustomObjects] = useState<JSONArray>([]);
-
-  const CommandValue = {
-    INSPECT_RECORD: "ins inspect record",
-    LIST_OBJECT: "lis list object",
-  };
 
   React.useEffect(() => {
     const fetchUsers = async () => {
@@ -51,13 +34,6 @@ export default function SalesforceCommandContext() {
 
     fetchCustomObjects();
   }, []);
-
-  function setRecordIdFromSearch(search: string): void {
-    const last18chars = search.substring(search.length - 18);
-    const newRecordId = isProbablySalesforceId(last18chars) ? last18chars : "";
-
-    setRecordId(newRecordId);
-  }
 
   return (
     <SalesforceCommand
