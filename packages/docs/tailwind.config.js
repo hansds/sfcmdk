d47d2aa63a23eeac4a036cadd635e81067f43472
Hans De Smet <hello@hansdesmet.be>
2023-09-18 19:52:08 +0200

    fix: dark mode background

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
     "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-light":
+          "radial-gradient(circle at top, #fdfafc , #d7dff1), url('/images/hero-bg-light.jpg')",
+        "hero-dark":
+          "radial-gradient(circle at top, #100036 , #030b1e), url('/images/hero-bg-dark.jpg')",
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
