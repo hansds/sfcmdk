e3f3dfcd4e0d2ea7c6353f722a461f1482f468f3
Hans De Smet <hello@hansdesmet.be>
2023-09-24 11:13:13 +0200

    test: add a rerender on input benchmark

--- packages/extension/src/pages/content/components/salesforceCommand/index.test.tsx
+++ packages/extension/src/pages/content/components/salesforceCommand/index.test.tsx
@@ -1,23 +1,58 @@
-import { render, screen } from "@testing-library/react";
-import App from "@src/pages/content/components/salesforceCommand";
-import { describe, test } from "@jest/globals";
+import { describe, jest, test } from "@jest/globals";
+import { act, render } from "@testing-library/react";
+import SalesforceCommand from ".";
+
+class ResizeObserver {
+  observe() {
+    return jest.fn();
+  }
+  unobserve() {
+    return jest.fn();
+  }
+  disconnect() {
+    return jest.fn();
+  }
+}
 
 describe("appTest", () => {
-  test("render text", () => {
-    // given
-    const text = "content view";
+  window.ResizeObserver = ResizeObserver;
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
-    // when
-    render(
-      <App
+  test("Benchmark rendering time of SalesforceCommand with updated input", () => {
+    const startTime = performance.now();
+    const { rerender } = render(
+      <SalesforceCommand
         users={[]}
         customObjects={[]}
         orgId="dummyOrgId"
-        sendMessage={chrome.runtime.sendMessage}
+        sendMessage={(message) => Promise.resolve({ type: message.type })}
       />
     );
 
-    // then
-    screen.getByText(text);
+    // Update the input or props that trigger a re-render
+    act(() => {
+      rerender(
+        <SalesforceCommand
+          users={[]}
+          customObjects={[]}
+          orgId="dummyOrgId"
+          sendMessage={(message) => Promise.resolve({ type: message.type })}
+          input="sandbox"
+        />
+      );
+    });
+
+    const endTime = performance.now();
+    const renderTime = endTime - startTime;
+
+    if (renderTime > 500) {
+      console.warn(
+        "SalesforceCommand took more than 500ms to render with updated input. Consider optimizing the component."
+      );
+    } else {
+      console.info(
+        `SalesforceCommand took ${renderTime}ms to render with updated input.`
+      );
+    }
   });
 });
